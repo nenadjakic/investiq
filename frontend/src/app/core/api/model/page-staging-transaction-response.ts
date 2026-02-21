81e719334c3e8ce2ee271e052186e1a57bd62699
nenadjakic <89663559+nenadjakic@users.noreply.github.com>
2026-02-21 10:07:18 +0100

    feat: add name to asset response

--- frontend/src/app/core/api/model/page-staging-transaction-response.ts
+++ frontend/src/app/core/api/model/page-staging-transaction-response.ts
@@ -20,9 +20,9 @@ export interface PageStagingTransactionResponse {
     number?: number;
     sort?: SortObject;
     pageable?: PageableObject;
+    numberOfElements?: number;
     first?: boolean;
     last?: boolean;
-    numberOfElements?: number;
     empty?: boolean;
 }
 
